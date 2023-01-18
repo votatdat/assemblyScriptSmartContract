e4d52fe29b20e11ae962aa9f6b31bd855cefec84
Vo Tat Dat <tatdat13@gmail.com>
2023-01-18 03:01:59 +0000

    Created Greeting contract

---
+++ assembly/index.ts
@@ -0,0 +1,10 @@
+import { Context, storage } from "near-sdk-as";
+
+export class Greeting {
+  getGreeting(accountId: string): string | null {
+    return storage.get<string>(accountId, "Greeting not available");
+  }
+  setGreeting(greeting: string): void {
+    storage.set<string>(Context.sender, greeting);
+  }
+}
